77108d5381fa937b56d0afcf257fe2efea249403
Kahfi <mkahfi467>
2023-04-12 15:13:21 +0700

    Progress kelas week 7

--- app/src/main/java/com/example/adv160420043week4/util/util.kt
+++ app/src/main/java/com/example/adv160420043week4/util/util.kt
@@ -1,9 +1,19 @@
 package com.example.adv160420043week4.util
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.adv160420043week4.R
+import com.example.adv160420043week4.view.MainActivity
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
@@ -27,3 +37,42 @@ fun ImageView.loadImage(url: String?, progressBar: ProgressBar) {
             }
         })
 }
+
+// WEEK 7
+fun showNotification(title:String, content:String, icon:Int) {
+    val channelId = "${MainActivity.instance?.packageName}-${MainActivity.instance?.getString(R.string.app_name)}"
+
+    val notificationBuilder = NotificationCompat.Builder(MainActivity.instance!!.applicationContext,channelId).apply {
+        setSmallIcon(icon)
+        setContentTitle(title)
+        setContentText(content)
+        setStyle(NotificationCompat.BigTextStyle())
+        priority = NotificationCompat.PRIORITY_DEFAULT
+        setAutoCancel(true)
+    }
+
+    val notificationManager = NotificationManagerCompat.from(MainActivity.instance!!.applicationContext.applicationContext!!)
+
+    if (ActivityCompat.checkSelfPermission(
+            MainActivity.instance!!.applicationContext,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+
+        return
+    }
+    notificationManager.notify(1001, notificationBuilder.build())
+}
+
+fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
+        channel.setShowBadge(showBadge)
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+// END WEEK 7
--- app/src/main/java/com/example/adv160420043week4/view/MainActivity.kt
+++ app/src/main/java/com/example/adv160420043week4/view/MainActivity.kt
@@ -1,13 +1,80 @@
 package com.example.adv160420043week4.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.adv160420043week4.R
+import com.example.adv160420043week4.util.createNotificationChannel
+import com.example.adv160420043week4.util.showNotification
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    // WEEK 7
+    init {
+        instance = this
+    }
+    companion object {
+        var instance:MainActivity ?= null
+    }
+    // END WEEK 7
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // WEEK 7
+        createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT, false, getString(R.string.app_name), "App notification channel.")
+
+        val fab = findViewById<FloatingActionButton>(R.id.fabBtnNotif)
+        fab.setOnClickListener {
+            val observable = Observable.timer(5, TimeUnit.SECONDS).apply {
+                subscribeOn(Schedulers.io())
+                observeOn(AndroidSchedulers.mainThread())
+                subscribe {
+                    Log.d("ObserverMessages", "Five second")
+                    showNotification("Dummy", "A new notification created", R.drawable.baseline_email_24)
+                }
+            }
+        }
+
+        val observable = Observable.just("a stream of data","hellow","world")
+
+        val observer = object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d("Messages", "begin subscribe")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("Messages", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("Messages", "complete")
+            }
+
+            override fun onNext(t: String) {
+                Log.d("Messages", "data: $t")
+            }
+
+        }
+
+        observable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer)
+
+        // END WEEK 7
     }
 }
